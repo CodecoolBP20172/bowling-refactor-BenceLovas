06d0f8c97730a4cb58e11c2944fe62c7e94b6d38
Bence Lovas <bansoul1@gmail.com>
2017-07-10 22:12:14 +0200

    unnecessary code removed

--- bowling.py
+++ bowling.py
@@ -17,12 +17,11 @@ def score(game):
                 else:
                     result += get_value(game[i + 2])
         last = get_value(game[i])
-        if not in_first_half:
+        if in_first_half is False:
             frame += 1
-        if in_first_half is True:
-            in_first_half = False
-        else:
             in_first_half = True
+        elif in_first_half is True:
+            in_first_half = False
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
