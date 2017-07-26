4794b02e846e470f14f06d70e742b576fdde4107
Bence Lovas <bansoul1@gmail.com>
2017-07-26 03:53:21 +0200

    data_processing func added to avoid further string convertions

--- bowling.py
+++ bowling.py
@@ -1,4 +1,6 @@
 def score(game):
+
+    game = data_processing(game)
     result = 0
     frame = 1
     in_first_half = True
@@ -10,7 +12,7 @@ def score(game):
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i] == 'x':
                 result += get_value(game[i + 1])
                 if game[i + 2] == '/':
                     result += 10 - get_value(game[i + 1])
@@ -22,10 +24,14 @@ def score(game):
             in_first_half = True
         elif in_first_half is True:
             in_first_half = False
-        if game[i] == 'X' or game[i] == 'x':
+        if game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
+
+
+def data_processing(data_string):
+    return data_string.lower()
 
 
 def get_value(char):
@@ -33,7 +39,7 @@ def get_value(char):
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
         return int(char)
-    elif char == 'X' or char == 'x':
+    elif char == 'x':
         return 10
     elif char == '/':
         return 10
